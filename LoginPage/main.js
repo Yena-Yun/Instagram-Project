14879003ce5991a5953ce3f12b3bbb04ab6f5ff1
Yena-Yun <cheryleduart@gmail.com>
2022-01-08 15:05:31 +0900

    feat: input창 span 글자 올리기, 비밀번호 표시 또는 숨기기 작업중

--- LoginPage/main.js
+++ LoginPage/main.js
@@ -13,3 +13,51 @@ setInterval(() => {
 
   if (index === imageArray.length) index = 0;
 }, 2000);
+
+const userInputDiv = document.querySelector('.username input');
+const passwordInputDiv = document.querySelector('.password input');
+const userPlaceholder = document.querySelector('.placeholder.username');
+const pwdPlaceholder = document.querySelector('.placeholder.password');
+const pwdInput = document.querySelector('.pwd-input input');
+console.log(pwdInput);
+
+const UP = 'up';
+
+userInputDiv.addEventListener('input', (e) => {
+  const v = e.target.value;
+
+  if (v) {
+    userPlaceholder.classList.add(UP);
+  } else {
+    userPlaceholder.classList.remove(UP);
+  }
+});
+
+passwordInputDiv.addEventListener('input', (e) => {
+  const v = e.target.value;
+
+  if (v) {
+    pwdPlaceholder.classList.add(UP);
+  } else {
+    pwdPlaceholder.classList.remove(UP);
+  }
+});
+
+const showPassword = document.querySelector('.show-password');
+console.log(showPassword.textContent);
+
+console.log(pwdInput.getAttribute('type'));
+
+showPassword.addEventListener('click', () => {
+  pwdInput.setAttribute('type', 'text');
+
+  console.log(pwdInput.getAttribute('type'));
+
+  showPassword.textContent = '숨기기';
+
+  // console.log(pwdInput.getAttribute('type'));
+  if (pwdInput.getAttribute('type') === 'text') {
+    pwdInput.setAttribute('type', 'password');
+    showPassword.textContent = '비밀번호 표시';
+  }
+});

ee3503ee8063ed6add5821059bbf1358d8b1cce2
Yena-Yun <cheryleduart@gmail.com>
2022-01-09 18:31:35 +0900

    feat: 친구리스트 슬라이드 구현

--- MainPage/main.js
+++ MainPage/main.js
@@ -1,7 +1,35 @@
+// 게시글 더보기 line-clamp
 const moreContent = document.querySelector('.more-content');
 const postDesc = document.querySelector('.post-desc');
 
 moreContent.addEventListener('click', () => {
-	postDesc.classList.toggle('more-initial');
-	moreContent.classList.add('none');
+  postDesc.classList.toggle('more-initial');
+  moreContent.classList.add('none');
+});
+
+// 이미지 슬라이드
+const slideContainer = document.querySelector('.slide-container');
+const leftBtn = document.querySelector('.left-slide-btn');
+const rightBtn = document.querySelector('.right-slide-btn');
+
+leftBtn.addEventListener('click', () => {
+  slideContainer.scrollLeft -= 268;
+  leftBtn.style.display = 'none';
+});
+
+leftBtn.style.display = 'none';
+
+rightBtn.addEventListener('click', () => {
+  slideContainer.scrollLeft += 268;
+});
+
+slideContainer.addEventListener('scroll', () => {
+  if (slideContainer.scrollLeft > slideContainer.scrollWidth - slideContainer.clientWidth - 1) {
+    rightBtn.style.display = 'none';
+  } else if (slideContainer.scrollLeft === 0) {
+    leftBtn.style.display = 'none';
+  } else {
+    rightBtn.style.display = 'block';
+    leftBtn.style.display = 'block';
+  }
 });
